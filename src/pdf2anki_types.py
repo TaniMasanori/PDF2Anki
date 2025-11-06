03927c0f11e106d96d6dc06b9d93f7ffe9028629
Masanori Tani <masnaori.tani.t@gmail.com>
2025-11-06 13:43:09 -0700

    Change license from MIT to GPL v3
    - Update LICENSE file to GNU General Public License v3.0
    - Add GPL v3 license headers to all source files
    - Update README.md with GPL v3 license information
    - Add license analysis documentation
    
    This change is required due to GPL-3.0-licensed dependencies
    (marker-api and marker-pdf) which require the entire project
    to be licensed under GPL v3 for license compatibility.

--- src/pdf2anki_types.py
+++ src/pdf2anki_types.py
@@ -3,6 +3,21 @@ Data type definitions for PDF2Anki conversion pipeline.
 
 This module defines the core data structures used throughout the conversion process,
 from PDF input to Anki card generation.
+
+Copyright (C) 2025  Masanori Tani
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 from dataclasses import dataclass, field
